f4c573fd312b8b3c89b4164b7f8831245c4c390b
luigidis <luigi.distefano69@gmail.com>
2022-08-24 15:46:37 +0200

    Initialized repo

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+// console.log('slider')
+
+const slides = [
+	'./img/01.jpg',
+	'./img/02.jpg',
+	'./img/03.jpg',
+	'./img/04.jpg',
+	'./img/05.jpg',
+]
+
+const app = new Vue ({
+	el: '#root',
+	data: {
+		slides,
+		active: true,
+	},
+	methods: {
+		
+	},
+})
